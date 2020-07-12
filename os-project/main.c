f214182e68a87cd630c12b16a971be8d8e6443fd
Hyeyoung <khyaa3966@naver.com>
2020-07-12 23:38:30 +0900

    Initial Commit

---
+++ os-project/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  os-project
+//
+//  Created by 김혜영 on 2020/07/12.
+//  Copyright © 2020 김혜영. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
